35b0216954c58caaf4717ad0e7ef93aacfe37dd9
AyeChanPyaeThu <ayechanpyaethu.it@gmail.com>
2025-06-13 13:21:53 +0630

    first commit

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title>AP Laravel</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css">
+</head>
+<h2 style="text-align:center">Sample Blog Site</h2>
+<body>
+   
+     @yield('content') 
+</body>
+</html>
