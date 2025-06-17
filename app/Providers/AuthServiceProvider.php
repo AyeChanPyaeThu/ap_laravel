4bb3211dcfa0ea97e415b1fea2587db35242054a
AyeChanPyaeThu <ayechanpyaethu.it@gmail.com>
2025-06-17 22:24:01 +0630

    week5 end

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -13,7 +14,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+         'App\Models\Post' => 'App\Policies\PostPolicy',
     ];
 
     /**
@@ -24,7 +25,13 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+        
+    //     Gate::define('view', function ( $user,  $post) {
+    //     return $user->id === $post->user_id;
+    // });
 
-        //
+//     Gate::before(function ( $user,) {
+//     return $user->id === 2;
+// });
     }
 }
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,6 +2,7 @@
 <head>
     <title>AP Laravel</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css">
+    <!-- <link rel="stylesheet" href="css/app.css"> -->
 </head>
 <h2 style="text-align:center">Sample Blog Site</h2>
 <body>
